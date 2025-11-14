2b2e0cbf507e6601bd6142f5dc236d6b0f4d2873
Levente Anda <andalevente2003@gmail.com>
2025-11-14 12:54:45 +0100

    [#3] Displaying a Triangle in MetalView, setting projection, view, model matrices

---
+++ Kase3D/Models/Shapes/Triangle.swift
@@ -0,0 +1,37 @@
+//
+//  Triangle.swift
+//  Kase3D
+//
+//  Created by Anda Levente on 2025. 11. 14..
+//
+
+import Foundation
+import MetalKit
+
+final class Triangle: Shape {
+    var transform: Transform
+    
+    var vertices: [Vertex] = []
+    var indices: [UInt32] = []
+    var vertexBuffer: MTLBuffer?
+    var indexBuffer: MTLBuffer?
+    
+    var device: MTLDevice
+    
+    init(device: MTLDevice) {
+        self.device = device
+        self.transform = Transform()
+        setupGeometry()
+        updateBuffers()
+    }
+    
+    func setupGeometry() {
+        vertices = [
+            Vertex(position: SIMD3<Float>(0.0, 0.5, 0.0)),    // top
+            Vertex(position: SIMD3<Float>(-0.5, -0.5, 0.0)),  // bottom-left
+            Vertex(position: SIMD3<Float>(0.5, -0.5, 0.0))    // bottom-right
+        ]
+        
+        indices = [0, 1, 2]
+    }
+}
--- Kase3D/Rendering/Renderer.swift
+++ Kase3D/Rendering/Renderer.swift
@@ -8,11 +8,15 @@
 import Foundation
 import MetalKit
 
-final class Renderer: NSObject, Sendable {
+@MainActor
+final class Renderer: NSObject {
     let device: MTLDevice
     let commandQueue: MTLCommandQueue
     let library: MTLLibrary
     let pipelineState: MTLRenderPipelineState
+    
+    var model: Drawable
+    var uniforms = Uniforms()
     
     init?(metalView: MTKView) {
         guard let device = MTLCreateSystemDefaultDevice(),
@@ -25,6 +29,8 @@ final class Renderer: NSObject, Sendable {
         self.commandQueue = commandQueue
         metalView.device = device
         
+        self.model = Triangle(device: device)
+        
         let vertexFunction = library.makeFunction(name: "vertex_main")
         let fragmentFunction = library.makeFunction(name: "fragment_main")
         
@@ -34,8 +40,8 @@ final class Renderer: NSObject, Sendable {
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
         pipelineDescriptor.vertexFunction = vertexFunction
         pipelineDescriptor.fragmentFunction = fragmentFunction
-        
         pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
+        pipelineDescriptor.vertexDescriptor = model.vertexDescriptor
         
         do {
             self.pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
@@ -46,6 +52,13 @@ final class Renderer: NSObject, Sendable {
         
         super.init()
         
+        uniforms.viewMatrix = float4x4(
+            [            1,             0,             0, 0],
+            [            0,             1,             0, 0],
+            [            0,             0,             1, 0],
+            [            0,             0,            -3, 1]
+        )
+        
         metalView.clearColor = MTLClearColor(
             red: 0.1,
             green: 0.1,
@@ -55,11 +68,28 @@ final class Renderer: NSObject, Sendable {
         
         metalView.delegate = self
     }
+    
+    func createProjectionMatrix(aspectRatio: Float) -> float4x4 {
+        let fov = Float.pi / 2
+        let near: Float = 0.1
+        let far: Float = 100.0
+        
+        let f = 1.0 / tan(fov / 2.0)
+        
+        var matrix = float4x4()
+        matrix[0, 0] = f / aspectRatio
+        matrix[1, 1] = f
+        matrix[2, 2] = (far + near) / (near - far)
+        matrix[2, 3] = -1.0
+        matrix[3, 2] = (2 * far * near) / (near - far)
+        
+        return matrix
+    }
 }
 
 extension Renderer: MTKViewDelegate {
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        //
+        
     }
     
     func draw(in view: MTKView) {
@@ -69,7 +99,6 @@ extension Renderer: MTKViewDelegate {
             return
         }
 
-        // Configure render pass (clear is handled by view.clearColor)
         renderPassDescriptor.colorAttachments[0].loadAction = .clear
         renderPassDescriptor.colorAttachments[0].storeAction = .store
 
@@ -77,11 +106,19 @@ extension Renderer: MTKViewDelegate {
             commandBuffer.commit()
             return
         }
-
-        // Set pipeline state; no vertex buffers yet (just a clear)
+        
         renderEncoder.setRenderPipelineState(pipelineState)
+        
+        let projectionMatrix = createProjectionMatrix(aspectRatio: Float((view.bounds.width / view.bounds.height)))
+        uniforms.projectionMatrix = projectionMatrix
 
-        // End encoding and present
+        model.position.y = -0.6
+        uniforms.modelMatrix = model.transform.modelMatrix
+        
+        renderEncoder.setVertexBytes(&uniforms, length: MemoryLayout<Uniforms>.size, index: 11)
+        
+        model.render(encoder: renderEncoder)
+
         renderEncoder.endEncoding()
         commandBuffer.present(drawable)
         commandBuffer.commit()
