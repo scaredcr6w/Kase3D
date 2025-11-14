0bea74deda0a666e11f672c4f1b5b9cb695d5f22
Levente Anda <85457088+scaredcr6w@users.noreply.github.com>
2025-11-14 07:38:08 +0100

    Initial Commit

---
+++ Kase3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kase3D
+//
+//  Created by Anda Levente on 2025. 11. 14..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kase3D/Kase3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kase3DApp.swift
+//  Kase3D
+//
+//  Created by Anda Levente on 2025. 11. 14..
+//
+
+import SwiftUI
+
+@main
+struct Kase3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
